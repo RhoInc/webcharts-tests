e258e69e003003a1d3c0ee2f1630c44f67cf3fca
samussiah <samussiah@gmail.com>
2018-08-03 09:16:31 -0400

    add render chart button

--- config-tester/configTester.js
+++ config-tester/configTester.js
@@ -15,15 +15,28 @@
 
         this.containers.configuration.append('h1').classed('ct-row__header', true).text('Configuration');
 
-        this.containers.settingsControl = this.containers.configuration.append('div').classed('ct-control ct-control--settings', true);
-        this.containers.controls.settings = this.containers.settingsControl.append('select').classed('ct-control__select', true);
-        this.containers.settingsControl.append('span').classed('ct-control__label', true).text('Chart Configuration');
+        //this.containers.settingsControl = this.containers.configuration
+        //    .append('div')
+        //    .classed('ct-control ct-control--settings', true);
+        //    this.containers.controls.settings = this.containers.settingsControl
+        //        .append('select')
+        //        .classed('ct-control__select', true);
+        //    this.containers.settingsControl
+        //        .append('span')
+        //        .classed('ct-control__label', true)
+        //        .text('Chart Configuration');
 
-        this.containers.dataControl = this.containers.configuration.append('div').classed('ct-control ct-control--data', true);
+        this.containers.buttons = this.containers.configuration.append('div').classed('ct-control-div ct-control-div--buttons', true);
+        this.containers.renderControl = this.containers.buttons.append('div').classed('ct-control ct-control--render', true);
+        this.containers.controls.render = this.containers.renderControl.append('button').classed('ct-control__button', true).text('Render Chart');
+
+        this.containers.dropdowns = this.containers.configuration.append('div').classed('ct-control-div ct-control-div--dropdowns', true);
+
+        this.containers.dataControl = this.containers.dropdowns.append('div').classed('ct-control ct-control--data', true);
         this.containers.controls.data = this.containers.dataControl.append('select').classed('ct-control__select', true);
         this.containers.dataControl.append('span').classed('ct-control__label', true).text('Data');
 
-        this.containers.branchesControl = this.containers.configuration.append('div').classed('ct-control ct-control--branches', true);
+        this.containers.branchesControl = this.containers.dropdowns.append('div').classed('ct-control ct-control--branches', true);
         this.containers.controls.branches = this.containers.branchesControl.append('select').classed('ct-control__select', true);
         this.containers.branchesControl.append('span').classed('ct-control__label', true).text('Webcharts Branch');
 
