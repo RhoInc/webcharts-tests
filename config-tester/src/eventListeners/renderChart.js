8e39d1b81ea9af2aff322f6cdc717d6351913abb
Spencer Childress <samussiah@gmail.com>
2019-05-16 12:43:47 -0400

    initialize chart conditionally

--- config-tester/src/eventListeners/renderChart.js
+++ config-tester/src/eventListeners/renderChart.js
@@ -6,26 +6,30 @@ import prepareChart from './renderChart/prepareChart';
 import updateSettings from './renderChart/updateSettings';
 import init from './renderChart/init';
 
-export default function renderChart() {
+export default function renderChart(init = true) {
     this.containers.controls.render.on('click', () => {
         //reset.call(this);
 
-        //Get current dropdown selections.
-        getConfiguration.call(this);
+        if (init) {
+            //Get current dropdown selections.
+            getConfiguration.call(this);
 
-        //Copy chart settings.
-        copyChartSettings.call(this);
+            //Copy chart settings.
+            copyChartSettings.call(this);
 
-        //Define table object.
-        prepareTable.call(this);
+            //Update settings textareas.
+            updateSettings.call(this);
 
-        //Define chart object.
-        prepareChart.call(this);
+            //Define table object.
+            prepareTable.call(this);
 
-        //Update settings textareas.
-        updateSettings.call(this);
+            //Define chart object.
+            prepareChart.call(this);
 
-        //Read in data and initialize table and chart.
-        init.call(this);
+            //Read in data and initialize table and chart.
+            init.call(this);
+        } else {
+            this.chart.draw();
+        }
     });
 }
